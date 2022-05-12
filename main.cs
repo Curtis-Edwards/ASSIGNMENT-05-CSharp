5848e6be835122643136869b30e9a1f034b2e75b
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-12 16:43:20 -0400

    added c# code

--- main.cs
+++ main.cs
@@ -5,19 +5,38 @@ class Program
     public static void Main(string[] args)
     {
         // This function accepts user input
-        int radius;
-        double pi = Math.PI;
-        double area;
+        int integer;
+        int currentsymbol = "negative";
+        int bottomnumber = 0 ;
+        int answer = 0 ;
 
-        Console.WriteLine("Please enter the radius of the circle");
+
+        Console.WriteLine("Please enter the number of iterations");
         Console.WriteLine("");
 
-        Console.Write("radius: ");
-        radius = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Number of iterations: ");
+        integer = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        area = (pi * radius) * radius;
 
-        Console.WriteLine("Area: " + area.ToString("0.00") + "cm²");
+        if (integer > 1)
+        {
+            for (let counter = 1; counter < integer; counter++)
+            {
+                if (currentSymbol == "negative")
+                {
+                    bottomNumber = bottomNumber + 2;
+                    answer = answer - (4 / bottomNumber);
+                    currentSymbol = "positive";
+                } else if (currentSymbol == "positive")
+                {
+                    bottomNumber = bottomNumber + 2;
+                    answer = answer + (4 / bottomNumber);
+                    currentSymbol = "negative";
+                }
+            }
+        }
+
+        Console.WriteLine(answer.ToString("0.0000000000"));
 
         Console.WriteLine("\nDone.");
     }
