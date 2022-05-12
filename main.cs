cb32d7728fc41ee3b2530858c516837d4c2c8540
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-12 21:20:24 +0000

    removed a single whitespace

--- main.cs
+++ main.cs
@@ -26,7 +26,7 @@ class Program
                     bottomnumber = bottomnumber + 2;
                     answer = answer - (4 / bottomnumber);
                     currentsymbol = "positive";
-                } 
+                }
                 else if (currentsymbol == "positive")
                 {
                     bottomnumber = bottomnumber + 2;
